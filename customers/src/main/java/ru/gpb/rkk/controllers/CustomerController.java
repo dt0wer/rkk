fe43149e11c34fc5f697a97af040af94ccf4e778
Maksim <maksim.stsiapanau@gmail.com>
2018-08-01 16:34:59 +0300

    check client side balancing

--- customers/src/main/java/ru/gpb/rkk/controllers/CustomerController.java
+++ customers/src/main/java/ru/gpb/rkk/controllers/CustomerController.java
@@ -3,6 +3,7 @@ package ru.gpb.rkk.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,10 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.naming.ServiceUnavailableException;
+import java.net.InetAddress;
 import java.net.URI;
+import java.net.UnknownHostException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -21,7 +25,8 @@ public class CustomerController {
     private RestTemplate loadbalancedRestTemplate;
 
     public Optional<URI> serviceUrl() {
-        return discoveryClient.getInstances("customers")
+        List<ServiceInstance> instances =  discoveryClient.getInstances("application");
+        return instances
                 .stream()
                 .map(si -> si.getUri()).findFirst();
     }
@@ -35,17 +40,25 @@ public class CustomerController {
 
     @RequestMapping("/")
     public String home() {
-        return "Customer service version: " + getVersion;
+
+        String ip = null;
+        try {
+             ip  = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "IP "+ip+" ;Customer service version: " + getVersion;
     }
 
     @RequestMapping("/discoveryClient")
     public String discoveryPing() throws RestClientException,
             ServiceUnavailableException {
-        URI service = serviceUrl()
-                .map(s -> s.resolve("/"))
-                .orElseThrow(ServiceUnavailableException::new);
-        return this.loadbalancedRestTemplate.getForEntity(service, String.class)
-                .getBody();
+//        URI service = serviceUrl()
+//                .map(s -> s.resolve("/application/info"))
+//                .orElseThrow(ServiceUnavailableException::new);
+
+        return this.loadbalancedRestTemplate.getForObject("http://customers/", String.class);
+
     }
 
 }
