0de701f202621c23b285b3d90d3818b15ddc2690
Maksim <maksim.stsiapanau@gmail.com>
2018-08-02 14:39:28 +0300

    fabio research

--- customers/src/main/java/ru/gpb/rkk/Application.java
+++ customers/src/main/java/ru/gpb/rkk/Application.java
@@ -2,11 +2,14 @@ package ru.gpb.rkk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class Application {
 
     public static void main(String[] args) {
