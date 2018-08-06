478489160230a29420014178649eb866b2d621fc
Aleksandr Zinovev <uplink.std@gmail.com>
2018-08-06 08:16:46 +0300

    Пример, обмена сообщениями между сервисами.

--- applications/src/main/java/ru/Application.java
+++ applications/src/main/java/ru/Application.java
@@ -3,6 +3,7 @@ package ru;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RemoteApplicationEventScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
