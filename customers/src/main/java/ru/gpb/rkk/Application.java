3e0cc50c3933409aa5ba5e985c60411b75167eec
Maksim <maksim.stsiapanau@gmail.com>
2018-08-09 12:07:28 +0300

    right depend_on
    minor fixes

--- customers/src/main/java/ru/gpb/rkk/Application.java
+++ customers/src/main/java/ru/gpb/rkk/Application.java
@@ -1,17 +1,10 @@
 package ru.gpb.rkk;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.consul.discovery.ConsulDiscoveryProperties;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import ru.gpb.rkk.config.VaultConfiguration;
 
 
 @SpringBootApplication
